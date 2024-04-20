764c94c49f6c0d2e55e9eba945e88620fc87c04f
KaBrz23 <kauefernandesbraz@gmail.com>
2024-04-20 14:21:25 -0300

    Adicionando teste parametrizado com CSVSource para consultar Distritos

--- src/test/java/org/estudos/br/ConsultaIBGETest.java
+++ src/test/java/org/estudos/br/ConsultaIBGETest.java
@@ -2,6 +2,8 @@ package org.estudos.br;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -10,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConsultaIBGETest {
     private static final String ESTADOS_API_URL = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/";
+    private static final String DISTRITOS_API_URL = "https://servicodados.ibge.gov.br/api/v1/localidades/distritos/";
 
 
     @Test
@@ -30,4 +33,25 @@ public class ConsultaIBGETest {
         int statusCode = connection.getResponseCode();
         assertEquals(200, statusCode, "O status code da resposta da API deve ser 200 (OK)");
     }
+
+    @ParameterizedTest
+    @CsvSource({"520005005", "310010405", "520010005"})
+    @DisplayName("Teste para consulta de distritos com CsvSource")
+    public void testConsultarDistrito(int identificador) throws IOException {
+        // Arrange
+        int id = identificador; // Define o estado a ser consultado
+
+        // Act
+        String resposta = ConsultaIBGE.consultarDistrito(id); // Chama o método a ser testado
+
+        // Assert
+        // Verifica se a resposta não está vazia
+        assert !resposta.isEmpty();
+
+        // Verifica se o status code é 200 (OK)
+        HttpURLConnection connection = (HttpURLConnection) new URL(DISTRITOS_API_URL + id).openConnection();
+        int statusCode = connection.getResponseCode();
+        assertEquals(200, statusCode, "O status code da resposta da API deve ser 200 (OK)");
+    }
+
 }
